a3e20331902b0c0a60f0688894a8e47f112ef7e9
Patrick Kao <patrick.d.kao@gmail.com>
2020-02-22 23:51:01 -0500

    two working swords

--- esp/config.h
+++ esp/config.h
@@ -2,8 +2,8 @@
 #define CONFIG_H
 
 // Server URL
-char URL[] = "http://65ac765d.ngrok.io/post";
-char HOST[] = "http://65ac765d.ngrok.io";
+char URL[] = "http://a8880110.ngrok.io/post";
+char HOST[] = "http://a8880110.ngrok.io";
 
 // Wifi constants
 char NETWORK[] = "stratus";  // SSID for Wifi Network
@@ -22,8 +22,11 @@ char body[600];
 float LP_SPEED = 0.95;  
 
 // thresholds
-float OPP_THRESH = 70;
-float SWORD_THRESH = 85;
+float OPP_THRESH1 = 70;
+float SWORD_THRESH1 = 85;
+float OPP_THRESH2 = 110;
+float SWORD_THRESH2 = 135;
 
+long MESSAGE_COOLDOWN = 1000;
 
 #endif
--- esp/esp.ino
+++ esp/esp.ino
@@ -3,9 +3,12 @@
 #include "config.h"
 #include <CapacitiveSensor.h>
 
-
-CapacitiveSensor cs_4_2 = CapacitiveSensor(16, 14); // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
-LowPass lp(LP_SPEED);
+long c1Time = 0;
+long c2Time=0;
+CapacitiveSensor c1 = CapacitiveSensor(16, 14); // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
+CapacitiveSensor c2 = CapacitiveSensor(13, 12);
+LowPass lp1(LP_SPEED);
+LowPass lp2(LP_SPEED);
 
 enum HitType {
   SWORD,
@@ -21,45 +24,60 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   setup_wifi();
-  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF); // turn off autocalibrate on channel 1 - just as an example Serial.begin(9600);
+  c1.set_CS_AutocaL_Millis(0xFFFFFFFF); // turn off autocalibrate on channel 1 - just as an example Serial.begin(9600);
+  c2.set_CS_AutocaL_Millis(0xFFFFFFFF);
 }
 void loop() {
   //put your main code here, to run repeatedly:
-  HitType event = detectEvent();
-  if (event != NONE) {
-    sprintf(body, "data=p1,%s", HitString[event]);
+  HitType event1 = detectEvent(&lp1, &c1);
+  if (event1 != NONE && millis()>c1Time+MESSAGE_COOLDOWN) {
+    sprintf(body, "data=p1,%s", HitString[event1]);
     http_post(body, requestBuffer, responseBuffer);
+    c1Time = millis();
     //    Serial.println(responseBuffer);
   }
+
+  HitType event2 = detectEvent(&lp2, &c2);
+  if (event2 != NONE && millis()>c2Time+MESSAGE_COOLDOWN) {
+    sprintf(body, "data=p2,%s", HitString[event2]);
+    http_post(body, requestBuffer, responseBuffer);
+    c2Time=millis();
+    //    Serial.println(responseBuffer);
+  }
+
+  Serial.print(senseCurrent(&c1));
+  Serial.print("\t");
+  Serial.println(senseCurrent(&c2));
 }
 
-long senseCurrent() {
-  long total1 = cs_4_2.capacitiveSensorRaw(30);
+long senseCurrent(CapacitiveSensor* sensor) {
+  long total1 = sensor->capacitiveSensorRaw(30);
   return total1;
 }
 
-HitType detectEvent() {
-  long currentReading = senseCurrent();
-  long low = lp.step(currentReading);
+HitType detectEvent(LowPass* lp, CapacitiveSensor* sensor) {
+  long currentReading = senseCurrent(sensor);
+  long low = lp->step(currentReading);
 
-  Serial.print(currentReading); // print sensor output 1
-
-  Serial.print("\t"); // tab character for debug window spacing
-
-  Serial.print(40);
-  Serial.print("\t"); // tab character for debug window spacing
-
-  Serial.print(100);
-  Serial.print("\t"); // tab character for debug window spacing
-
-  Serial.println(low);
-
-
-  if (currentReading > SWORD_THRESH) {
-    return SWORD;
-  } else if (low > OPP_THRESH) {
-    return OPPONENT;
+  if (sensor == &c1) {
+    if (currentReading > SWORD_THRESH1) {
+      return SWORD;
+    } else if (low > OPP_THRESH1) {
+      return OPPONENT;
+    } else {
+      return NONE;
+    }
+  } else if(sensor==&c2) {
+        if (currentReading > SWORD_THRESH2) {
+      return SWORD;
+    } else if (low > OPP_THRESH2) {
+      return OPPONENT;
+    } else {
+      return NONE;
+    }
   } else{
+    Serial.println("Something very wrong");
     return NONE;
   }
+
 }
