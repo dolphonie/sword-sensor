2cca1802b77882378737df53f79bf7da75c503f1
Tamique de Brito <51467831+tamique-debrito@users.noreply.github.com>
2020-02-22 20:38:19 -0500

    Update RunningSum.c

--- esp/RunningSum.c
+++ esp/RunningSum.c
@@ -8,7 +8,7 @@ struct RunningAvg{
 		current_index = 0;
 	}
 
-	float Average(){ # Use to get current value of smoothed signal
+	float Average(){ // Use to get current value of smoothed signal
 		float sum = 0;
 		for(int i = 0;  i < WINDOW_SIZE; i++) {
 			sum += vals[i];
@@ -16,7 +16,7 @@ struct RunningAvg{
 		return sum / WINDOW_SIZE;
 	}
 
-	void input_sample(float sample) { #Call this function on samples as you get them
+	void input_sample(float sample) { // Call this function on samples as you get them
 		current_index = (current_index + 1) % WINDOW_SIZE;
 		samples[current_index] = sample;
 	}
